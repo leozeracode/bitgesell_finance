3428795b3debddac683c492b90243607aacbc174
Leonardo Viana <lviana@solvd.com>
2025-06-28 16:45:53 -0300

    test: add get items controller test

---
+++ backend/src/application/controllers/get-items-controller.spec.ts
@@ -0,0 +1,58 @@
+import { MainController, GetItemsController } from '@/application/controllers'
+import { GetItems } from '@/domain/usecases'
+
+import { mock, MockProxy } from 'jest-mock-extended'
+import { mockItems } from '@/domain/test'
+import { ServerError } from '@/application/errors'
+import { faker } from '@faker-js/faker'
+
+describe('GetItemsController', () => {
+  let getItems: MockProxy<GetItems>
+  let sut: GetItemsController
+  let filter: GetItems.Input
+  let output: GetItems.Output
+
+  beforeAll(() => {
+    filter = { query: faker.string.alpha(10), limit: 10 }
+    output = mockItems(3)
+
+    getItems = mock()
+    getItems.get.mockResolvedValue(output)
+  })
+
+  beforeEach(() => {
+    sut = new GetItemsController(getItems)
+  })
+
+  it('should extends Controller', () => {
+    expect(sut).toBeInstanceOf(MainController)
+  })
+
+  it('should call GetItems correctly', async () => {
+    await sut.handle(filter)
+
+    expect(getItems.get).toHaveBeenCalled()
+    expect(getItems.get).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return 500 on infra error', async () => {
+    const error = new Error('infra_error')
+    getItems.get.mockRejectedValueOnce(error)
+
+    const httpResponse = await sut.handle(filter)
+
+    expect(httpResponse).toEqual({
+      statusCode: 500,
+      data: new ServerError(error)
+    })
+  })
+
+  it('should return 200 if add succeeds', async () => {
+    const httpResponse = await sut.handle(filter)
+
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      data: output
+    })
+  })
+})
